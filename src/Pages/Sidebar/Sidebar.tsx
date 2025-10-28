c99b92b15799622ec85f7d9fc44efc5d5aeaa87b
Vadim <_s_e_t__@ukr.net>
2025-10-28 12:08:01 +0200

    add some code

--- src/Pages/Sidebar/Sidebar.tsx
+++ src/Pages/Sidebar/Sidebar.tsx
@@ -1,5 +1,24 @@
 import "./Sidebar.css";
+import materials from "../../data/materials.json";
+
+interface Material {
+  title: string;
+  body: string;
+  href: string;
+}
 
 export const Sidebar = () => {
-  return <div className='Sidebar'></div>;
+  return (
+    <div className="Sidebar">
+      <nav>
+        <ul>
+          {materials.map((item: Material, idx) => (
+            <li key={idx}>
+              <a href={item.href}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
